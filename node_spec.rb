70a8be10b79535457f9c6f5653278bce835d2b10
Katrina <blueyed_qt@hotmail.com>
2015-11-08 12:45:12 -0800

    Set up for SLL complete

---
+++ node_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'node'
+
+RSpec.describe Node do 
+	before do 
+		@node = Node.new(1)
+	end
+
+	it "initializes with one argument to se the element attribute" do 
+		expect(@node.element).to eq(1)
+	end
+
+	it "has a next instance variable with default value of nil" do 
+		expect(@node.next).to eq(nil)
+	end
+end
